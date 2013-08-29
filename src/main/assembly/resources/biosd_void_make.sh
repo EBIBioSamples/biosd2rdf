706f34284d7db84d4a05924d85b4e8659558ec3b
Marco Brandizi <brandizi@ebi.ac.uk>
2013-08-29 17:06:39 +0100

    VOID Generation Tool Added.

---
+++ src/main/assembly/resources/biosd_void_make.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# This is the entry point that invokes the BioSD-VOID creation line command.
+# 
+
+# Do you use a proxy?
+if [ "$http_proxy" != '' ]; then
+  OPTS="$OPTS -DproxySet=true -DproxyHost=wwwcache.ebi.ac.uk -DproxyPort=3128 -DnonProxyHosts='*.ebi.ac.uk|localhost'"
+fi
+
+# These are passed to the JVM. they're appended, so that you can predefine your stuff via export OPTS=...
+# on my laptop: OPTS="$OPTS -Xms4G -Xmx8G -XX:PermSize=256m -XX:MaxPermSize=512m"
+#OPTS="$OPTS -Xms16G -Xmx32G -XX:PermSize=512m -XX:MaxPermSize=1G"
+
+# We always work with universal text encoding.
+OPTS="$OPTS -Dfile.encoding=UTF-8"
+
+# Monitoring with jconsole (end-user doesn't usually need this)
+#OPTS="$OPTS 
+# -Dcom.sun.management.jmxremote.port=5010
+# -Dcom.sun.management.jmxremote.authenticate=false
+# -Dcom.sun.management.jmxremote.ssl=false"
+       
+# Used for invoking a command in debug mode (end user doesn't usually need this)
+#OPTS="$OPTS -Xdebug -Xnoagent"
+#OPTS="$OPTS -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
+
+
+# You shouldn't need to change the rest
+#
+###
+
+cd "$(dirname $0)"
+MYDIR="$(pwd)"
+
+CP="$MYDIR:$MYDIR/${project.artifactId}_deps.jar"
+if [ "$JDBCPATH" != "" ]; then
+  CP="$CP:$JDBCPATH"
+fi
+
+# See here for an explanation about ${1+"$@"} :
+# http://stackoverflow.com/questions/743454/space-in-java-command-line-arguments 
+
+java \
+  $OPTS -cp $CP uk.ac.ebi.fg.biosd.biosd2rdf.BiosdVoidCmd ${1+"$@"}
+
+EXCODE=$?
+
+echo Java Finished. Quitting the Shell Too. >&2
+exit $EXCODE
---
+++ src/main/java/uk/ac/ebi/fg/biosd/biosd2rdf/BiosdVoidCmd.java
@@ -0,0 +1,137 @@
+package uk.ac.ebi.fg.biosd.biosd2rdf;
+
+import static java.lang.System.err;
+import static java.lang.System.out;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.text.StrSubstitutor;
+
+/**
+ * TODO: Comment me!
+ * 
+ * biosd_void_make.sh.sh [--template <path>] [--properties <property-file>] [output-path]
+ *
+ * <dl><dt>date</dt><dd>17 Jul 2013</dd></dl>
+ * @author Marco Brandizi
+ *
+ */
+public class BiosdVoidCmd
+{
+	
+	public static void main ( String[] args ) throws Throwable
+	{
+		int exCode = 0;
+		CommandLine cli = null;
+		
+		try
+		{
+			CommandLineParser clparser = new GnuParser ();
+			cli = clparser.parse ( getOptions(), args );
+
+			if ( cli.hasOption ( 'h' ) ) throw new ParseException ( "--help" );
+
+			args = cli.getArgs ();
+			
+			String tplPath = cli.getOptionValue ( 't', "./biosd_void_template.ttl" );
+			String propPath = cli.getOptionValue ( 'p' );
+			
+			Properties props;
+			if ( propPath == null ) props = System.getProperties ();
+			else
+			{
+				props = new Properties ();
+				props.load ( new FileReader ( new File ( propPath ) ) );
+			}
+			
+			String result = StrSubstitutor.replace ( IOUtils.toString ( new FileReader ( new File ( tplPath ) ) ), props );
+			PrintStream out = args.length > 0 
+				? new PrintStream ( new FileOutputStream ( new File ( args [ 0 ] ) ) )
+				: System.out;
+			
+			out.print ( result );
+			out.close ();
+		} 
+		catch ( Throwable ex ) 
+		{
+			if ( ex instanceof ParseException ) 
+			{
+				if ( !"--help".equals ( ex.getMessage () ) )
+					err.println ( "\n  Command syntax error: " + ex.getMessage () + "\n\n");
+				
+				printUsage ();
+				exCode = 128;
+			}
+			else 
+			{
+				ex.printStackTrace( System.err );
+				exCode = 1; // TODO: proper exit code management
+			}
+		}
+		finally 
+		{
+			System.exit ( exCode );
+		}
+	}
+	
+	
+	private static void printUsage ()
+	{
+		out.println ();
+
+		out.println ( "\n\n *** BioSD RDF Exporter ***" );
+		out.println ( "\nCreates a void file, merging a template with some release-specific parameters, taken from java properties." );
+		
+		out.println ( "Syntax:" );
+		out.println ( "\n\tbiosd_void_make.sh [options] [output-path]\n\n" );
+
+		err.println ( "\nOptions:" );
+		
+		HelpFormatter helpFormatter = new HelpFormatter ();
+		PrintWriter pw = new PrintWriter ( err, true );
+		helpFormatter.printOptions ( pw, 100, getOptions (), 2, 4 );
+		
+		out.println ( "\nStandard output is used if the output path is omitted.\n" );
+	}
+	
+	@SuppressWarnings ( { "static-access" } )
+	private static Options getOptions ()
+	{
+		Options opts = new Options ();
+		
+		opts.addOption ( OptionBuilder
+			.withDescription ( "Template file (default is ./biosd_void_template.ttl" )
+			.hasArg ().withArgName ( "<path>" )
+			.withLongOpt ( "template" )
+			.create ( 't' )
+		);
+
+		opts.addOption ( OptionBuilder
+			.withDescription ( ".properties file containing template's placeholder values (uses -D parameters if omitted)" )
+			.hasArg ().withArgName ( "<path>" )
+			.withLongOpt ( "properties" )
+			.create ( 'p' )
+		);
+
+		opts.addOption ( OptionBuilder
+			.withDescription ( "Prints out this message" )
+			.withLongOpt ( "help" )
+			.create ( 'h' )
+		);
+
+		return opts;		
+	}
+}
