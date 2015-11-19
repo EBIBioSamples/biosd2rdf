6d805671723a4a9d0146aace954bb5c5fa29c0ee
Marco Brandizi <brandizi@ebi.ac.uk>
2015-11-19 23:40:54 +0000

    Adding script to make FTP dumps.

---
+++ src/main/assembly/resources/make_ftp_dump.sh
@@ -0,0 +1,25 @@
+version=$1
+if [ "$version" == "" ]; then
+
+	cat <<EOT
+
+
+	Usage: $(basename $0) <dataset-version>
+
+Must be run after 'become bsd-svc'. Zips the contents of data/ (i.e., the RDF files uploaded on Virtuoso) to create 
+a tar.bz2 file in our FTP space.
+
+EOT
+
+	exit 1
+fi
+
+archive_base_name=biosd_rdf_$version
+
+# Parallel BZIP2, much faster, very cool! http://compression.ca/pbzip2
+PBZIP2=/homes/brandizi/local/bin/pbzip2
+cd $(dirname $0)
+tar cv data --transform="s|^data|$archive_base_name|" \
+  | $PBZIP2 --stdout >/ebi/ftp/pub/databases/biosamples/biosd2rdf/$archive_base_name.tar.bz2
+
+echo 'The End'
