9f3132c067915982274b03fe3da66529027adfcc
Marco Brandizi <brandizi@ebi.ac.uk>
2016-02-04 15:16:12 +0000

    More automation about EBI deployment.

--- zip_deploy.sh
+++ zip_deploy.sh
@@ -21,11 +21,23 @@ echo
 echo "_______________ Deploying the Command Line Binary ($version) to $target _________________"
 
 # We need to remove old versions and unused libs
-rm -Rf "$target/biosd2rdf_cmdline_${version}/lib"
+rm -Rf "$target/biosd2rdf_cmdline_${version}/{lib,rdf}"
 
 yes A| unzip biosd2rdf_cmdline_${version}.zip -d "$target"
 cp -f biosd2rdf_cmdline_${version}.zip "$target"
-chmod -R ug=rwX,o=rX "$target/biosd2rdf_cmdline_${version}" "$target/biosd2rdf_cmdline_${version}.zip"
+chmod -R ug=rwX,o=rX "$target/biosd2rdf_cmdline_${version}" 
+chmod ugo=rwX "$target/biosd2rdf_cmdline_${version}.zip"
+
+
+new_src="$target/biosd2rdf_cmdline_${version}"
+target=/net/isilonP/public/rw/homes/rdf_adm/biosamples/biosd2rdf_cmdline
+
+echo
+echo 
+echo "_______________ Deploying Virtuoso Scripts ($version) to $target _________________"
+
+/bin/cp -R -d --preserve=all --verbose "$new_src/ebi_deploy" "$target"
+chmod -R ugo=rwX "$target"
 
 echo ______________________________________________________________________________
 echo
